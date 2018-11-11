49bafa0b631747bb4db32a4041fce17dc4a8966a
shin1rok <skssikm@gmail.com>
2018-11-12 08:53:40 +0900

    decimal型で小数点以下の桁数を設定する場合は、全体の桁数の設定も必要

--- db/migrate/20181111131528_create_companies.rb
+++ db/migrate/20181111131528_create_companies.rb
@@ -6,7 +6,7 @@ class CreateCompanies < ActiveRecord::Migration[5.2]
       t.string :email, null: false, commnet: 'email'
       t.string :tell, null: false, commnet: 'tell'
       t.integer :employees, null: false, 　default: 0, commnet: '従業員数'
-      t.decimal :capital, scale: 0, commnet: '資本金'
+      t.decimal :capital, precision: 11, scale: 0, commnet: '資本金'
       t.date :establish_date, null: false, commnet: '設立日'
       t.string :url, null: false, commnet: 'URL'
       t.float :average_age, null: false, commnet: '平均年齢'
