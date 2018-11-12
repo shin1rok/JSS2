689f3f98d1d3218c61bb3db8e3114376f264f8ee
shin1rok <skssikm@gmail.com>
2018-11-12 23:31:01 +0900

    devise JobSeeker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_124324) do
+ActiveRecord::Schema.define(version: 2018_11_12_141205) do
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -35,6 +35,32 @@ ActiveRecord::Schema.define(version: 2018_11_12_124324) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_job_offers_on_company_id"
+  end
+
+  create_table "job_seekers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_job_seekers_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_job_seekers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_job_seekers_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_job_seekers_on_unlock_token", unique: true
   end
 
   add_foreign_key "job_offers", "companies"
