afcbe142755fbd8b3f76c1d6a24b40d6a3cf2b62
shin1rok <skssikm@gmail.com>
2018-11-25 18:09:46 +0900

    TODO追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :profiles
   devise_for :recruiters
   devise_for :job_seekers
-  root 'companies#index'
+  root 'companies#index' # TODO ちゃんとする
   resources :job_offers
   resources :companies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
